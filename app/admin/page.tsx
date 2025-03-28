7c85c1f8e753ff20662b7e0596f0ad8a1ae1aa04
nyakonda anesu <nyakondaanesu@gmail.com>
2025-03-28 17:18:41 +0200

    added protected routes

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,16 @@
 import MessagesHome from "../(components)/messages";
-
-export default function Home() {
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+const AdminPage = async () => {
+  const session = await auth();
+  if (!session) {
+    redirect("/");
+  }
   return (
     <div className="container mx-auto p-6">
       <MessagesHome />
     </div>
   );
-}
+};
+
+export default AdminPage;
